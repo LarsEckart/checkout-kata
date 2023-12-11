c1197344a758036f37b05fad0378ae702cdcb849
Lars Eckart <lars.eckart@hey.com>
2023-12-11 13:07:10 +0200

    first discount rule implemented

--- src/test/java/kata/Checkout.java
+++ src/test/java/kata/Checkout.java
@@ -13,19 +13,23 @@ class Checkout {
 
     public void scan(SKU sku) {
         scannedItems.add(sku);
-        if (sku.equals(SKU.of("A"))) {
-            total = total.plus(Money.ofMinor(CurrencyUnit.USD, 50));
-        } else if (sku.equals(SKU.of("B"))) {
-            total = total.plus(Money.ofMinor(CurrencyUnit.USD, 30));
-        } else if (sku.equals(SKU.of("C"))) {
-            total = total.plus(Money.ofMinor(CurrencyUnit.USD, 20));
-        } else if (sku.equals(SKU.of("D"))) {
-            total = total.plus(Money.ofMinor(CurrencyUnit.USD, 15));
-        }
     }
 
     public Money total() {
-
+        for (SKU sku : scannedItems) {
+            if (sku.equals(SKU.of("A"))) {
+                total = total.plus(Money.ofMinor(CurrencyUnit.USD, 50));
+            } else if (sku.equals(SKU.of("B"))) {
+                total = total.plus(Money.ofMinor(CurrencyUnit.USD, 30));
+            } else if (sku.equals(SKU.of("C"))) {
+                total = total.plus(Money.ofMinor(CurrencyUnit.USD, 20));
+            } else if (sku.equals(SKU.of("D"))) {
+                total = total.plus(Money.ofMinor(CurrencyUnit.USD, 15));
+            }
+        }
+        if (scannedItems.stream().filter(sku -> sku.equals(SKU.of("A"))).count() >= 3) {
+            total = total.minus(Money.ofMinor(CurrencyUnit.USD, 20));
+        }
         return total;
     }
 }
