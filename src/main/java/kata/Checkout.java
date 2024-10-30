c87d9287f19cb564229d3caaf7195ce3959cf411
Lars Eckart <lars.eckart@hey.com>
2024-10-30 10:58:24 +0200

    wip

--- src/main/java/kata/Checkout.java
+++ src/main/java/kata/Checkout.java
@@ -22,13 +22,17 @@ class Checkout {
         for (Sku sku : cart) {
             total = total.plus(catalogue.getPriceFor(sku));
         }
-        if (cart.counts().containsKey(Sku.of("A")) && cart.counts().get(Sku.of("A")) >= 3) {
-            total = total.minus(Money.of(CurrencyUnit.USD, 20));
-        }
-        if (cart.counts().containsKey(Sku.of("B")) && cart.counts().get(Sku.of("B")) >= 2) {
-            total = total.minus(Money.of(CurrencyUnit.USD, 15));
-        }
+        total = total.minus(calculateDiscount(cart, Sku.of("A"), 3, Money.of(CurrencyUnit.USD, 20)));
+        total = total.minus(calculateDiscount(cart, Sku.of("B"), 2, Money.of(CurrencyUnit.USD, 15)));
+
         return total;
+    }
+
+    private Money calculateDiscount(Cart cart, Sku sku, int quantity, Money discount) {
+        if (cart.counts().containsKey(sku) && cart.counts().get(sku) >= quantity) {
+            return discount;
+        }
+        return Money.zero(CurrencyUnit.USD);
     }
 
 }
