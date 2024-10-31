32791c2bb8dd28bb1d14f78984800b4dfaf77c7e
Lars Eckart <lars.eckart@hey.com>
2024-10-31 14:03:08 +0200

    rename migrations (deleted checkout.db file)

--- src/main/java/kata/SqLiteCatalogue.java
+++ src/main/java/kata/SqLiteCatalogue.java
@@ -7,24 +7,26 @@ import org.joda.money.Money;
 import org.jooq.DSLContext;
 import org.jooq.Record1;
 import org.jooq.Result;
+import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
-import kata.jooq.tables.Item;
 import kata.jooq.tables.records.ItemRecord;
+
+import static kata.jooq.tables.Item.ITEM;
 
 class SqLiteCatalogue implements Catalogue {
 
     private final DSLContext dslContext;
 
     public SqLiteCatalogue(Connection connection) {
-        this.dslContext = DSL.using(connection);
+        this.dslContext = DSL.using(connection, SQLDialect.SQLITE);
     }
 
     @Override
     public Money getPriceFor(Sku sku) {
-        Result<Record1<ItemRecord>> fetch = dslContext.select(Item.ITEM)
-                .from(Item.ITEM)
-                .where(Item.ITEM.SKU.eq(sku.asString())).fetch();
+        Result<Record1<ItemRecord>> fetch = dslContext.select(ITEM)
+                .from(ITEM)
+                .where(ITEM.SKU.eq(sku.asString())).fetch();
 
         return fetch.isEmpty() ? null : Money.of(CurrencyUnit.USD, fetch.getFirst().value1().getPrice());
     }
