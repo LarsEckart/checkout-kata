108c28a3aa61d607bfa66538d7d4c952c946038e
Lars Eckart <lars.eckart@hey.com>
2025-11-06 14:15:51 +0200

    remove unused constructor

--- src/main/java/kata/ItemNotFoundException.java
+++ src/main/java/kata/ItemNotFoundException.java
@@ -7,8 +7,4 @@ public final class ItemNotFoundException extends RuntimeException {
     public ItemNotFoundException(String message) {
         super(message);
     }
-
-    public ItemNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
